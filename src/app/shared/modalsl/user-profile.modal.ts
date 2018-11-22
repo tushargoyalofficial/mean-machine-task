020a8ef0f15bf70d589b1568d4623440210b53b0
Bunny Molester <tushar462024@gmail.com>
2018-11-22 23:09:44 +0530

    Define user profile interface

---
+++ src/app/shared/modalsl/user-profile.modal.ts
@@ -0,0 +1,12 @@
+export interface UserProfile {
+  id?: string;
+  name?: string;
+  image?: string;
+  email?: string;
+  password?: string;
+  status?: string;
+  role?: string;
+  username?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
