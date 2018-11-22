8650fd7996cb2dc9ff8747a70b3de6147feaed60
Bunny Molester <tushar462024@gmail.com>
2018-11-22 13:47:07 +0530

    Create Account service file and declared login and signup methods which will further process by interacting with API

--- src/app/services/account/account.service.ts
+++ src/app/services/account/account.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { SERVERURL } from '../config/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+
+  // LOGIN METHOD TO INTERACT WITH SERVER API
+  loginOnServer(body: Object): Observable<any> {
+    const url: string = SERVERURL + '/Accounts/login';
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        // 'Authorization': fooBarToken
+      }),
+      withCredentials: true // for set-cookie to work
+     };
+    // const headers = new HttpHeaders({
+    //   'Content-Type': 'application/json'
+    // });
+    return this.http.post(url, body, httpOptions);
+  }
+
+
+
+
+  // SIGNUP METHOD TO INTERACT WITH SERVER API
+  signupOnServer(body: Object): Observable<any> {
+    const url: string = SERVERURL + '/Accounts';
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        // 'Authorization': fooBarToken
+      }),
+      // withCredentials: true // for set-cookie to work
+     };
+    // const headers = new HttpHeaders({
+    //   'Content-Type': 'application/json'
+    // });
+    return this.http.post(url, body, httpOptions);
+  }
 }
