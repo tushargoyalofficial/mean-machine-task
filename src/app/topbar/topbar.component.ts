33c3fb66033aae240ce0fde176086b9cad800bc0
Bunny Molester <tushar462024@gmail.com>
2018-11-22 21:38:56 +0530

    Create menu dropdown, show realtime updated data of user.

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -6,6 +6,8 @@ import { CartService } from '../services/cart/cart.service';
 import { Product } from '../shared/modalsl/product.modal';
 import { SignoutDialogComponent } from '../shared/dialogs/signout-dialog/signout-dialog.component';
 import { CartProduct } from '../shared/modalsl/cart-product.modal';
+import { AccountService } from '../services/account/account.service';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-topbar',
@@ -17,15 +19,19 @@ export class TopbarComponent implements OnInit, OnDestroy {
   // FOR UNSUBSCRIBING FROM SUBSCRIPTION
   private ngUnsubscribe: Subject<boolean> = new Subject<boolean>();
 
+  public userId: string;
+  public userName: string;
+  public imageURL: string;
+
   // CART PRODUCTS OBSERVABLE FOR REALTIME UPDATE
   public cartItems$: Observable<Product[]>; // SUBSCRIBE TO PRODUCT CART ITEMS
   public getTotalAmount$: Observable<number>; // SUBSCRIBE TO PRODUCT CART TOTAL AMOUNT
-
   public products: Array<Product> = [];
 
   constructor(
     private router: Router,
     private dialog: MatDialog,
+    private accountService: AccountService,
     private cartService: CartService
   ) { }
 
@@ -65,6 +71,23 @@ export class TopbarComponent implements OnInit, OnDestroy {
 
 
   ngOnInit() {
+    this.imageURL = 'assets/img/profileImg.png';
+    this.accountService.userId
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(userId => this.userId = userId);
+    this.accountService.userName
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(name => this.userName = name);
+    this.accountService.userImage
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(img => {
+        if (img) {
+          this.imageURL = img;
+        } else {
+          this.imageURL = 'assets/img/profileImg.png';
+        }
+      });
+
     this.cartItems$ = this.cartService.getCartItems();
     this.getTotalAmount$ = this.cartService.getCartTotalAmount();
     this.cartItems$.subscribe((products: CartProduct[]) => {
