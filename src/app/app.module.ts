3fc297936ca951186c2084588196acde2826e3c9
Bunny Molester <tushar462024@gmail.com>
2018-11-22 18:25:16 +0530

    Add missing declarations and providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // ROUTING MODULE
 import { AppRoutingModule } from './app-routing.module';
@@ -18,12 +19,13 @@ import { ProfileComponent } from './profile/profile.component';
 import { ProductsComponent } from './products/products.component';
 
 // TEMPLATE
+import { TopbarComponent } from './topbar/topbar.component';
 import { ProductCardComponent } from './shared/template/product-card/product-card.component';
 
 // ALL SERVICES
 import { AccountService } from './services/account/account.service';
 import { ProductService } from './services/product/product.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CartService } from './services/cart/cart.service';
 
 
 
@@ -34,7 +36,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SignupComponent,
     ProfileComponent,
     ProductsComponent,
-    ProductCardComponent
+    TopbarComponent,
+    ProductCardComponent,
   ],
   imports: [
     BrowserModule,
@@ -48,7 +51,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   providers: [
     AccountService,
-    ProductService
+    ProductService,
+    CartService
   ],
   bootstrap: [AppComponent]
 })
