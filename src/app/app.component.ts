ccbad55af1a77d4c3240ee1067f287e96fccc065
Bunny Molester <tushar462024@gmail.com>
2018-11-22 15:24:11 +0530

    Configure auto login feature to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AccountService } from './services/account/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ui-panel';
+export class AppComponent implements OnInit {
+  title = 'Mean Machine Task App';
+
+  constructor(
+    private router: Router,
+    private snackbar: MatSnackBar,
+    private accountService: AccountService
+  ) { }
+
+
+  private autoLogin(): void {
+    const accessToken = localStorage.getItem('authToken');
+    const userId = localStorage.getItem('userId');
+    if (accessToken) {
+      this.accountService.checkUserInfo(accessToken, userId)
+        .subscribe((res: any) => {
+          if (res.id && res.role && res.status === 'active') {
+            this.accountService.userAuthentic(true);
+            this.accountService.setRole(res.role);
+            this.router.navigate(['products']);
+          } else {
+            this.accountService.userAuthentic(false);
+            this.accountService.setRole(null);
+            localStorage.clear();
+            this.router.navigate(['login']);
+          }
+        }, err => { console.log('Error in access token: ', err); });
+    }
+  }
+
+
+  ngOnInit() {
+    this.autoLogin();
+  }
 }
